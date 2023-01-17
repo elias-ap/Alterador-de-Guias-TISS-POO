e9706519b17c1d775a921422da462fb11f7e41b7
Elias Araujo Pedroso <elias.ap.pro@gmail.com>
2023-01-17 11:11:37 -0300

    Commit

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 from poo import *
-import pandas as pd
 
-# READ WORKSHEET TABLE OF DATA ALTERATION
-tabelaDeCriticas = pd.read_excel("Planilha de Críticas.xlsx", sheet_name='Dados', dtype=str, keep_default_na=False)
-
-conta = ContaTiss()
+conta = Conta()
 tabela = TabelaDeCritica()
 for linha in tabela.getTabelaDados():
-    setLinhaAlteracaoDeDados(linha)
+    numero_guia, codigoDeProcedimento, novoCodigoDeProcedimento, tipoDeTabela, novoTipoDeTabela, \
+    grauDeParticipacao, novoGrauDeParticipacao, codigoDeDesepsa, novoCodigoDeDespesa, unidadeDeMedida, \
+    novoUnidadeDeMedida = setLinhaAlteracaoDeDados(linha)
 
     for item in conta.guias:
-        guia = Guia(item, codProc)
+        guia = Guia(item, codigoDeProcedimento)
 
         print('----------- ' + guia.getNumeroGuia() + ' -----------')
         if Et.iselement(guia.getProcedimentosExecutados()):
             for procedimento in guia.getListaProcedimento():
                 p = Procedimento(procedimento, guia)
-                p.alteraCodigoProcedimento(codProc, novoCodProc)
-                p.alteraGrauParticipacao()
-                p.alteraCodigoTabela()
-                p.alteraValorUnitario()
+                p.alteraCodigoProcedimentoExecutado(codigoDeProcedimento, novoCodigoDeProcedimento)
+                p.alteraGrauDeParticipacao(grauDeParticipacao, novoGrauDeParticipacao)
+                p.alteraCodigoDeTabela(tipoDeTabela, novoTipoDeTabela)
 
-        if Et.iselement(guia.getOutrasDespesas()):
+        if Et.iselement(guia.getDespesas()):
             for procedimento in guia.getListaDespesa():
                 p = Procedimento(procedimento, guia)
-                p.alteraCodigoProcedimento(codProc, novoCodProc)
-                p.alteraCodigoDespesa()
-                p.alteraCodigoTabela()
-                p.alteraUnidadeMedida()
-                p.alteraValorUnitario()
+                p.alteraCodigoProcedimentoDespesa(codigoDeProcedimento, novoCodigoDeProcedimento)
+                p.alteraCodigoDeDespesa(codigoDeDesepsa, novoCodigoDeDespesa)
+                p.alteraCodigoDeTabela(tipoDeTabela, novoTipoDeTabela)
+                p.alteraUnidadeDeMedida(unidadeDeMedida, novoUnidadeDeMedida)
 
+for linha in tabela.getTabelaValores():
+    numero_guia, codigoDeProcedimento, valor, novoValor = setLinhaAlteracaoDeValores(linha)
 
+    for item in conta.guias:
+        guia = Guia(item, codigoDeProcedimento)
 
-        else:
-            print('nao existe outras despesas nessa conta')
+        print('----------- ' + guia.getNumeroGuia() + ' -----------')
+        if Et.iselement(guia.getProcedimentosExecutados()):
+            for procedimento in guia.getListaProcedimento():
+                p = Procedimento(procedimento, guia)
+                p.alteraValorUnitario(valor, novoValor)
+
+        if Et.iselement(guia.getDespesas()):
+            for procedimento in guia.getListaDespesa():
+                p = Procedimento(procedimento, guia)
+                p.alteraValorUnitario(valor, novoValor)
--- poo.py
+++ poo.py
@@ -1,52 +1,48 @@
-import typing
 import xml.etree.ElementTree as Et
 from pandas import read_excel, DataFrame
 
-codigoProcedimento = '41101014'
-codigoDespesa = '90289870'
+ans_prefix = {"ans": "http://www.ans.gov.br/padroes/tiss/schemas"}
 
 
-class ContaTiss:
-    corpoGuia: Et.Element
-    guias: typing.Generator
-    ans_prefix = {"ans": "http://www.ans.gov.br/padroes/tiss/schemas"}
+class Conta:
+    corpo_guia: Et.Element
+    guias: [Et.Element]
 
     def __init__(self):
-        caminhoGuia = r"00000000000000011306_92a0e8826a52304e3ac85dfe30c83f38.xml"
-        self.corpoGuia = Et.parse(caminhoGuia, parser=Et.XMLParser(encoding="ISO-8859-1")).getroot()
-        self.guias = self.corpoGuia.find('.//ans:guiasTISS', self.ans_prefix)
+        caminho_guia = r"00000000000000011306_92a0e8826a52304e3ac85dfe30c83f38.xml"
+        self.corpo_guia = Et.parse(caminho_guia, parser=Et.XMLParser(encoding="ISO-8859-1")).getroot()
+        self.guias = self.corpo_guia.find('.//ans:guiasTISS', ans_prefix)
 
-    def setCorpoGuia(self, corpo):
-        self.setCorpoGuia(corpo)
+    def setCorpoGuia(self, corpo_guia):
+        self.setCorpoGuia(corpo_guia)
 
     def getCorpoGuia(self):
-        return self.corpoGuia
+        return self.corpo_guia
 
 
 class Guia:
-    numeroGuia: Et.ElementTree
+    numero_guia: Et.ElementTree
     guia: Et.ElementTree
-    procedimentosExecutados: Et.ElementTree
-    outrasDespesas: Et.ElementTree
-    dadosProcedimento: [Et.ElementTree]
-    dadosDespesa: [Et.ElementTree]
-    ans_prefix = ContaTiss.ans_prefix
-    proc: str
+    procedimentos: Et.ElementTree
+    despesas: Et.ElementTree
+    lista_de_procedimentos: [Et.ElementTree]
+    lista_de_despesas: [Et.ElementTree]
+    procedimento: str
 
     def __init__(self, guia, p):
         self.setNumeroGuia(guia)
         self.setGuia(guia)
-        self.proc = p
+        self.procedimento = p
         self.setProcedimentosExecutados(guia)
-        self.setOutrasDespesas(guia)
+        self.setDespesas(guia)
         self.setListaProcedimentos()
         self.setListaDespesa()
 
-    def setNumeroGuia(self, guia: str):
-        self.numeroGuia = guia.find('.//ans:cabecalhoGuia/ans:numeroGuiaPrestador', self.ans_prefix).text
+    def setNumeroGuia(self, guia: []):
+        self.numero_guia = guia.find('.//ans:cabecalhoGuia/ans:numeroGuiaPrestador', ans_prefix).text
 
     def getNumeroGuia(self):
-        return self.numeroGuia
+        return self.numero_guia
 
     def setGuia(self, guia):
         self.guia = guia
@@ -55,45 +51,45 @@ class Guia:
         return self.guia
 
     def setProcedimentosExecutados(self, guia):
-        self.procedimentosExecutados = guia.find('ans:procedimentosExecutados', self.ans_prefix)
+        self.procedimentos = guia.find('ans:procedimentosExecutados', ans_prefix)
 
     def getProcedimentosExecutados(self):
-        return self.procedimentosExecutados
+        return self.procedimentos
 
-    def setOutrasDespesas(self, guia):
-        self.outrasDespesas = guia.find('ans:outrasDespesas', self.ans_prefix)
+    def setDespesas(self, guia):
+        self.despesas = guia.find('ans:outrasDespesas', ans_prefix)
 
-    def getOutrasDespesas(self):
-        return self.outrasDespesas
+    def getDespesas(self):
+        return self.despesas
 
     def setListaProcedimentos(self):
         if Et.iselement(self.getProcedimentosExecutados()):
-            self.dadosProcedimento = list(self.getProcedimentosExecutados().iterfind(
-                f'.//ans:procedimento[ans:codigoProcedimento="{self.proc}"]..', self.ans_prefix))
+            self.lista_de_procedimentos = list(self.getProcedimentosExecutados().iterfind(
+                f'.//ans:procedimento[ans:codigoProcedimento="{self.procedimento}"]..', ans_prefix))
 
     def getListaProcedimento(self):
-        return self.dadosProcedimento
+        return self.lista_de_procedimentos
 
     def setListaDespesa(self):
-        if Et.iselement(self.getOutrasDespesas()):
-            self.dadosDespesa = list(self.getOutrasDespesas().iterfind(
-                f'.//ans:servicosExecutados[ans:codigoProcedimento="{self.proc}"]..', self.ans_prefix))
+        if Et.iselement(self.getDespesas()):
+            self.lista_de_despesas = list(self.getDespesas().iterfind(
+                f'.//ans:servicosExecutados[ans:codigoProcedimento="{self.procedimento}"]..', ans_prefix))
 
     def getListaDespesa(self):
-        return self.dadosDespesa
+        return self.lista_de_despesas
 
     def alteraValorTotalGeral(self, diferenca):
-        valoresTotais = self.getGuia().find('ans:valorTotal', self.ans_prefix)
-        valorTotalGeral = valoresTotais.find('ans:valorTotalGeral', self.ans_prefix)
+        valores_totais = self.getGuia().find('ans:valorTotal', ans_prefix)
+        valor_total_geral = valores_totais.find('ans:valorTotalGeral', ans_prefix)
 
-        for valorTotal in valoresTotais:
-            if float(valorTotal.text) > diferenca:
-                valorTotal.text = '%.2f' % (float(valorTotal.text) - diferenca)
-                valorTotalGeral.text = '%.2f' % (float(valorTotalGeral.text) - diferenca)
+        for valor_total in valores_totais:
+            if float(valor_total.text) > diferenca:
+                valor_total.text = '%.2f' % (float(valor_total.text) - diferenca)
+                valor_total_geral.text = '%.2f' % (float(valor_total_geral.text) - diferenca)
                 break
 
-        print(f"\n----- Valores totais -----\n\nValor total alterado para {valorTotal.text}")
-        print(f"Valor total geral alterado para {valorTotalGeral.text}\n")
+        print(f"\n----- Valores totais -----\n\nValor total alterado para {valor_total.text}")
+        print(f"Valor total geral alterado para {valor_total_geral.text}\n")
 
 
 class Procedimento:
@@ -110,91 +106,144 @@ class Procedimento:
     def getProcedimento(self):
         return self.procedimento
 
-    def alteraCodigoProcedimento(self, codProc, novoCodProc):
-        if len(self.guia.getListaProcedimento()) > 0:
-            codigo = self.getProcedimento().find('ans:procedimento/ans:codigoProcedimento', self.guia.ans_prefix)
-            if codigo.text == codProc and novoCodProc != '':
-                codigo.text = novoCodProc
-                print(f"Codigo de procedimento alterado de {codProc} para {novoCodProc}")
-
-        elif len(self.guia.getListaDespesa()) > 0:
-            codigo = self.getProcedimento().find('.//ans:codigoProcedimento', self.guia.ans_prefix)
-            if codigo.text == codProc and novoCodProc != '':
-                codigo.text = novoCodProc
-                print(f"Codigo de procedimento alterado de {codProc} para {novoCodProc}")
-
-    def alteraValorUnitario(self):
-        novoValorUnitario = 10
-        valorUnitario = self.getProcedimento().find('.//ans:valorUnitario', self.guia.ans_prefix)
-        quantidadeExecutada = float(
-            self.getProcedimento().find('.//ans:quantidadeExecutada', self.guia.ans_prefix).text)
-        valorTotal = self.getProcedimento().find('.//ans:valorTotal', self.guia.ans_prefix)
-        if float(valorUnitario.text) != novoValorUnitario:
-            novoValorTotal = float(novoValorUnitario * quantidadeExecutada)
-            if float(valorTotal.text) > novoValorTotal:
-                diferenca = (float(valorTotal.text) - novoValorTotal)
+    def podeAlterar(self):
+        if numero_guia != '':
+            if numero_guia == self.guia.getNumeroGuia():
+                return True
             else:
-                diferenca = (novoValorTotal - float(valorTotal.text))
+                return False
 
-            valorUnitario.text = '%.2f' % novoValorUnitario
-            valorTotal.text = '%.2f' % novoValorTotal
+        else:
+            return True
 
-            print(f"Valor unitario alterado para {valorUnitario.text}")
-            print(f"Valor total alterado para {valorTotal.text}")
+    def alteraCodigoProcedimentoExecutado(self, codigo, novo_codigo):
+        if self.podeAlterar():
+            if len(self.guia.getListaProcedimento()) > 0:
+                tag_codigo_de_procedimento = self.getProcedimento().find('ans:procedimento/ans:codigoProcedimento', ans_prefix)
+                if tag_codigo_de_procedimento.text == codigo and novo_codigo != '':
+                    tag_codigo_de_procedimento.text = novo_codigo
+                    print(f"Codigo de procedimento (executado) alterado de {codigo} para {novo_codigo}")
 
-            self.guia.alteraValorTotalGeral(diferenca)
+    def alteraCodigoProcedimentoDespesa(self, codigo, novo_codigo):
+        if self.podeAlterar():
+            if len(self.guia.getListaDespesa()) > 0:
+                tag_codigo_de_procedimento = self.getProcedimento().find('.//ans:codigoProcedimento', ans_prefix)
+                if tag_codigo_de_procedimento.text == codigo and novo_codigo != '':
+                    tag_codigo_de_procedimento.text = novo_codigo
+                    print(f"Codigo de procedimento (despesa) alterado de {codigo} para {novo_codigo}")
 
-    def alteraCodigoTabela(self):
-        codigoTabela = self.getProcedimento().find('.//ans:codigoTabela', self.guia.ans_prefix)
-        codigoTabela.text = '050'
-        print('Código de tabela alterado para: ' + codigoTabela.text)
+    def alteraValorUnitario(self, valor, novo_valor):
+        if self.podeAlterar():
+            tag_valor_unitario = self.getProcedimento().find('.//ans:valorUnitario', ans_prefix)
+            quantidade_executada = float(
+                self.getProcedimento().find('.//ans:quantidadeExecutada', ans_prefix).text)
+            valor_total = self.getProcedimento().find('.//ans:valorTotal', ans_prefix)
 
-    def alteraGrauParticipacao(self):
-        grauParticipacao = self.getProcedimento().find('.//ans:grauPart', self.guia.ans_prefix)
-        grauParticipacao.text = '20'
-        print('Grau de participação alterado para: ' + grauParticipacao.text)
+            if float(tag_valor_unitario.text) == valor and float(tag_valor_unitario.text) != novo_valor:
+                novoValorTotal = float(novo_valor * quantidade_executada)
+                if float(valor_total.text) > novoValorTotal:
+                    diferenca = (float(valor_total.text) - novoValorTotal)
+                else:
+                    diferenca = (novoValorTotal - float(valor_total.text))
 
-    def alteraCodigoDespesa(self):
-        codigoDespesa = self.getProcedimento().find('.//ans:codigoDespesa', self.guia.ans_prefix)
-        codigoDespesa.text = '06'
-        print('Código de despesa alterado para: ' + codigoDespesa.text)
+                tag_valor_unitario.text = '%.2f' % novo_valor
+                valor_total.text = '%.2f' % novoValorTotal
 
-    def alteraUnidadeMedida(self):
-        unidadeMedida = self.getProcedimento().find('.//ans:unidadeMedida', self.guia.ans_prefix)
-        unidadeMedida.text = '06'
-        print('Unidade de medida alterado para: ' + unidadeMedida.text)
+                print(f"Valor unitario alterado para {tag_valor_unitario.text}")
+                print(f"Valor total alterado para {valor_total.text}")
+
+                self.guia.alteraValorTotalGeral(diferenca)
+
+    def alteraCodigoDeTabela(self, codigo, novo_codigo):
+        if self.podeAlterar():
+            tag_codigo_tabela = self.getProcedimento().find('.//ans:codigoTabela', ans_prefix)
+            if tag_codigo_tabela.text == codigo and novo_codigo != '':
+                tag_codigo_tabela.text = novo_codigo
+                print('Código de tabela alterado para: ' + tag_codigo_tabela.text)
+
+    def alteraCodigoDeDespesa(self, codigo, novo_codigo):
+        if self.podeAlterar():
+            tag_codigo_despesa = self.getProcedimento().find('.//ans:codigoDespesa', ans_prefix)
+            if tag_codigo_despesa.text == codigo and novo_codigo != '':
+                tag_codigo_despesa.text = novo_codigo
+                print('Código de despesa alterado para: ' + tag_codigo_despesa.text)
+
+    def alteraGrauDeParticipacao(self, grau, novo_grau):
+        if self.podeAlterar():
+            tag_grau_participacao = self.getProcedimento().find('.//ans:grauPart', ans_prefix)
+            if tag_grau_participacao.text == grau and novo_grau != '':
+                tag_grau_participacao.text = novo_grau
+                print('Código de despesa alterado para: ' + tag_grau_participacao.text)
+
+    def alteraUnidadeDeMedida(self, unidade, nova_unidade):
+        if self.podeAlterar():
+            tag_unidade_de_medida = self.getProcedimento().find('.//ans:unidadeMedida', ans_prefix)
+            if tag_unidade_de_medida.text == unidade and nova_unidade != '':
+                tag_unidade_de_medida.text = nova_unidade
+                print('Unidade de medida alterado para: ' + tag_unidade_de_medida.text)
 
 
 class TabelaDeCritica:
-    tabelaDados: DataFrame
-    tabelaValores: DataFrame
+    tabela_de_dados: DataFrame
+    tabela_de_valores: DataFrame
 
     def __init__(self):
-        self.tabelaDados = read_excel("Planilha de Críticas.xlsx", sheet_name='Dados', dtype=str, keep_default_na=False)
-        self.tabelaDados = read_excel("Planilha de Críticas.xlsx", sheet_name='Dados', dtype=str, keep_default_na=False)
+        self.tabela_de_dados = read_excel("Planilha de Críticas.xlsx", sheet_name='Dados', dtype=str, keep_default_na=False)
+        self.tabela_de_valores = read_excel("Planilha de Críticas.xlsx", sheet_name='Valores', dtype=str,
+                                            keep_default_na=False)
+        self.formataDadosTabelaValores()
+        self.formataDadosTabelaDados()
+
+    def formataDadosTabelaValores(self):
+        coluna_valor_atual = self.tabela_de_valores['Valor unitário (atual)'].str.replace(',', '.')
+        coluna_valor_novo = self.tabela_de_valores['Valor unitário (novo)'].str.replace(',', '.')
+
+        self.tabela_de_valores['Valor unitário (atual)'] = coluna_valor_atual
+        self.tabela_de_valores['Valor unitário (novo)'] = coluna_valor_novo
+        self.tabela_de_valores['Valor unitário (atual)'] = coluna_valor_atual.astype(float)
+        self.tabela_de_valores['Valor unitário (novo)'] = coluna_valor_novo.astype(float)
+
+    def formataDadosTabelaDados(self):
+        for indice, texto in self.tabela_de_dados['Unidade de medida (novo)'].items():
+            tamanho_texto = len(texto)
+            if tamanho_texto == 2:
+                self.tabela_de_dados['Unidade de medida (novo)'][indice] = texto.rjust(tamanho_texto + 1, '0')
+
+        for indice, texto in self.tabela_de_dados['Tipo de tabela (novo)'].items():
+            if texto == '0':
+                texto = '00'
+                self.tabela_de_dados['Tipo de tabela (novo)'][indice] = texto
 
     def getTabelaDados(self):
-        return self.tabelaDados.values
+        return self.tabela_de_dados.values
 
     def getTabelaValores(self):
-        return self.tabelaValores.values
+        return self.tabela_de_valores.values
 
 
 def setLinhaAlteracaoDeDados(linha):
-    nonlocal numGuia, codProc, novoCodProc, codTabela, novoCodTabela, grauP, novoGrauP, codDespesa, novoCodDespesa,\
-        unidadeMedida, novoUnidadeMedida
-    numGuia = linha[0]
-    codProc = linha[1]
-    novoCodProc = linha[2]
-    codTabela = linha[3]
-    novoCodTabela = linha[4]
-    grauP = linha[5]
-    novoGrauP = linha[6]
-    codDespesa = linha[7]
-    novoCodDespesa = linha[8]
-    unidadeMedida = linha[9]
-    novoUnidadeMedida = linha[10]
+    global numero_guia
+    numero_guia = linha[0]
+    codigo_de_procedimento = linha[1]
+    novo_codigo_de_procedimento = linha[2]
+    codigo_de_tabela = linha[3]
+    novo_codigo_de_tabela = linha[4]
+    grau_de_participacao = linha[5]
+    novo_grau_de_participacao = linha[6]
+    codigo_de_desepsa = linha[7]
+    novo_codigo_de_despesa = linha[8]
+    unidade_de_medida = linha[9]
+    novo_unidade_de_medida = linha[10]
+
+    return numero_guia, codigo_de_procedimento, novo_codigo_de_procedimento, codigo_de_tabela, novo_codigo_de_tabela, \
+           grau_de_participacao, novo_grau_de_participacao, codigo_de_desepsa, novo_codigo_de_despesa, unidade_de_medida, \
+           novo_unidade_de_medida
 
 
+def setLinhaAlteracaoDeValores(linha):
+    numero_guia = linha[0]
+    codigo_procedimento = linha[1]
+    valor_unitario = linha[2]
+    novo_valor_unitario = linha[3]
 
-
+    return numero_guia, codigo_procedimento, valor_unitario, novo_valor_unitario
