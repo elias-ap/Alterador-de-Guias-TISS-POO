e748e23f3ecdbeb3a7f314ed1f216194ed55fde6
Elias Araujo Pedroso <elias.ap.pro@gmail.com>
2023-01-18 12:50:35 -0300

    commit

--- poo.py
+++ poo.py
@@ -1,6 +1,9 @@
 import hashlib
 import os
 import xml.etree.ElementTree as Et
+import customtkinter as ctk
+from tkinter import filedialog as fd
+from tkinter import messagebox as mb
 from pandas import read_excel, DataFrame
 
 ans_prefix = {"ans": "http://www.ans.gov.br/padroes/tiss/schemas"}
@@ -12,8 +15,8 @@ class Conta:
     tag_raiz: Et.Element
     guias: [Et.Element]
 
-    def __init__(self):
-        self.caminho_conta = r"00000000000000011306_92a0e8826a52304e3ac85dfe30c83f38.xml"
+    def __init__(self, caminho: str):
+        self.caminho_conta = caminho
         self.corpo_conta = Et.parse(self.caminho_conta, parser=Et.XMLParser(encoding="ISO-8859-1"))
         self.tag_raiz = self.corpo_conta.getroot()
         self.guias = self.tag_raiz.find('.//ans:guiasTISS', ans_prefix)
@@ -117,6 +120,7 @@ class Guia:
 class Procedimento:
     procedimento: Et.ElementTree
     guia: Guia
+    qtd_alteracoes: int = 0
 
     def __init__(self, procedimento, guia):
         self.setProcedimento(procedimento)
@@ -145,6 +149,7 @@ class Procedimento:
                                                                          ans_prefix)
                 if tag_codigo_de_procedimento.text == codigo and novo_codigo != '':
                     tag_codigo_de_procedimento.text = novo_codigo
+                    self.qtd_alteracoes += 1
                     print(f"Codigo de procedimento (executado) alterado de {codigo} para {novo_codigo}")
 
     def alteraCodigoProcedimentoDespesa(self, codigo, novo_codigo):
@@ -153,6 +158,7 @@ class Procedimento:
                 tag_codigo_de_procedimento = self.getProcedimento().find('.//ans:codigoProcedimento', ans_prefix)
                 if tag_codigo_de_procedimento.text == codigo and novo_codigo != '':
                     tag_codigo_de_procedimento.text = novo_codigo
+                    self.qtd_alteracoes += 1
                     print(f"Codigo de procedimento (despesa) alterado de {codigo} para {novo_codigo}")
 
     def alteraValorUnitario(self, valor, novo_valor):
@@ -171,7 +177,7 @@ class Procedimento:
 
                 tag_valor_unitario.text = '%.2f' % novo_valor
                 valor_total.text = '%.2f' % novoValorTotal
-
+                self.qtd_alteracoes += 1
                 print(f"Valor unitario alterado para {tag_valor_unitario.text}")
                 print(f"Valor total alterado para {valor_total.text}")
 
@@ -182,6 +188,7 @@ class Procedimento:
             tag_codigo_tabela = self.getProcedimento().find('.//ans:codigoTabela', ans_prefix)
             if tag_codigo_tabela.text == codigo and novo_codigo != '':
                 tag_codigo_tabela.text = novo_codigo
+                self.qtd_alteracoes += 1
                 print('Código de tabela alterado para: ' + tag_codigo_tabela.text)
 
     def alteraCodigoDeDespesa(self, codigo, novo_codigo):
@@ -189,6 +196,7 @@ class Procedimento:
             tag_codigo_despesa = self.getProcedimento().find('.//ans:codigoDespesa', ans_prefix)
             if tag_codigo_despesa.text == codigo and novo_codigo != '':
                 tag_codigo_despesa.text = novo_codigo
+                self.qtd_alteracoes += 1
                 print('Código de despesa alterado para: ' + tag_codigo_despesa.text)
 
     def alteraGrauDeParticipacao(self, grau, novo_grau):
@@ -196,6 +204,7 @@ class Procedimento:
             tag_grau_participacao = self.getProcedimento().find('.//ans:grauPart', ans_prefix)
             if tag_grau_participacao.text == grau and novo_grau != '':
                 tag_grau_participacao.text = novo_grau
+                self.qtd_alteracoes += 1
                 print('Código de despesa alterado para: ' + tag_grau_participacao.text)
 
     def alteraUnidadeDeMedida(self, unidade, nova_unidade):
@@ -203,10 +212,11 @@ class Procedimento:
             tag_unidade_de_medida = self.getProcedimento().find('.//ans:unidadeMedida', ans_prefix)
             if tag_unidade_de_medida.text == unidade and nova_unidade != '':
                 tag_unidade_de_medida.text = nova_unidade
+                self.qtd_alteracoes += 1
                 print('Unidade de medida alterado para: ' + tag_unidade_de_medida.text)
 
 
-class TabelaDeCritica:
+class Tabela:
     tabela_de_dados: DataFrame
     tabela_de_valores: DataFrame
 
@@ -238,11 +248,154 @@ class TabelaDeCritica:
                 texto = '00'
                 self.tabela_de_dados['Tipo de tabela (novo)'][indice] = texto
 
+
+# GUI COLOR CONFIG
+ctk.set_appearance_mode('dark')
+ctk.set_default_color_theme('green')
+
+
+class Interface:
+    # MAIN WINDOW
+    main_window: ctk.CTk()
+    # ctk.CTk.iconbitmap(main_window, r'Resources/icon.ico')
+
+    # MAIN FRAME
+    frame: ctk.CTkFrame
+
+    # DEFAULT BUTTON
+    open_plan_button: ctk.CTkButton
+
+    # RELATIVE BUTTONS
+    generateHashAndSave_button: ctk.CTkButton
+    botao_carregar_guia: ctk.CTkButton
+
+    # BUTTONS AFTER GUIDE CHOOSE
+    # cancel_button = ctk.CTkButton(frame, text='Cancelar')
+    # cancel_button.pack(side='bottom', pady=5, padx=5)
+
+    check_button_information: ctk.CTkLabel
+    alteration_button: ctk.CTkButton
+    data_alteration_check_button: ctk.CTkSwitch
+    value_alteration_check_button: ctk.CTkSwitch
+
+    conta: Conta
+    control_var: int
+
+    def __init__(self):
+        # MAIN WINDOW
+        self.main_window = ctk.CTk()
+        # ctk.CTk.iconbitmap(main_window, r'Resources/icon.ico')
+        self.main_window.geometry('300x260')
+        self.main_window.title('Alterador de Guias TISS')
+        self.main_window.eval('tk::PlaceWindow . center')
+        self.main_window.maxsize(300, 260)
+
+        # MAIN FRAME
+        self.frame = ctk.CTkFrame(self.main_window)
+        self.frame.pack(side='left', fill='both', padx=10, pady=10, expand=True)
+
+        # DEFAULT BUTTON
+        self.open_plan_button = ctk.CTkButton(self.frame, text='Abrir planilha')
+        self.open_plan_button.pack(side='bottom', pady=5, padx=5)
+
+        # RELATIVE BUTTONS
+        self.generateHashAndSave_button = ctk.CTkButton(self.frame, text='Gerar hash')
+        self.generateHashAndSave_button.pack(side='bottom', pady=5, padx=5)
+
+        self.botao_carregar_guia = ctk.CTkButton(self.frame, text='Carregar Guia',
+                                                 command=lambda: self.carregarGuia())
+        self.botao_carregar_guia.pack(side='bottom', pady=5, padx=5)
+
+        # BUTTONS AFTER GUIDE CHOOSE
+        # cancel_button = ctk.CTkButton(frame, text='Cancelar')
+        # cancel_button.pack(side='bottom', pady=5, padx=5)
+
+        self.check_button_information = ctk.CTkLabel(self.frame, text='Escolha os modos de alteração:')
+        self.check_button_information.pack(side='top', pady=5, padx=5)
+
+        self.alteration_button = ctk.CTkButton(self.frame, text='Realizar alterações',
+                                               command=lambda: self.realizarAlteracoes())
+        self.alteration_button.pack(side='bottom', pady=5, padx=5)
+
+        self.data_alteration_check_button = ctk.CTkSwitch(self.frame, text='Alteração de dados', text_color='white')
+        self.data_alteration_check_button.pack(pady=10, padx=5)
+
+        self.value_alteration_check_button = ctk.CTkSwitch(self.frame, text='Alteração de valor',
+                                                           text_color='white')
+        self.value_alteration_check_button.pack(side='top', padx=5, pady=10)
+
+    # DESTROY RELATIVE BUTTONS
+    def destroy(self):
+        self.generateHashAndSave_button.destroy()
+        self.botao_carregar_guia.destroy()
+
+    def createRelativeButtons(self):
+        global generateHashAndSave_button, chooseGuide_button
+
+        generateHashAndSave_button = ctk.CTkButton(self.frame, text='Gerar hash', command=lambda: print(1))
+        generateHashAndSave_button.pack(side='bottom', pady=5, padx=5)
+
+        chooseGuide_button = ctk.CTkButton(self.frame, text='Carregar Guia', command=lambda: print(1))
+        chooseGuide_button.pack(side='bottom', pady=5, padx=5)
+
+    def tipoDeAlteracao(self):
+        altera_dados = self.data_alteration_check_button.get()
+        altera_valores = self.value_alteration_check_button.get()
+        if altera_dados == 1:
+            altera_dados = True
+        else:
+            altera_dados = False
+
+        if altera_valores == 1:
+            altera_valores = True
+        else:
+            altera_valores = False
+
+        return altera_dados, altera_valores
+
+    def carregarGuia(self):
+        self.conta = carregarGuia()
+
+    def realizarAlteracoes(self):
+        altera_dados, altera_valores = self.tipoDeAlteracao()
+
+        if altera_dados == True:
+            alteraDados(self.conta)
+
+        if altera_valores == True:
+            alteraValores(self.conta)
+
+        if variavel_de_controle > 0:
+            for button in (self.alteration_button, self.value_alteration_check_button,
+                           self.data_alteration_check_button, self.check_button_information):
+                button.destroy()
+
+    def salvarGuia(self):
+        self.conta.salvarConta()
+
+    def cancelAlteration(self):
+        # REDEFINE BUTTONS
+        if 'control_var' not in globals():
+            for button in (
+                    self.alteration_button, self.cancel_button, self.data_alteration_check_button,
+                    self.value_alteration_check_button,
+                    self.check_button_information):
+                button.destroy()
+
+        elif self.control_var > 0:
+            for button in (self.saveGuide_button, self.cancel_button):
+                button.destroy()
+
     def getTabelaDados(self):
         return self.tabela_de_dados.values
 
     def getTabelaValores(self):
         return self.tabela_de_valores.values
+
+
+def foiAlterado():
+    global control_var
+    control_var += 1
 
 
 def setLinhaAlteracaoDeDados(linha):
@@ -272,3 +425,69 @@ def setLinhaAlteracaoDeValores(linha):
     novo_valor_unitario = linha[3]
 
     return numero_guia, codigo_procedimento, valor_unitario, novo_valor_unitario
+
+
+def carregarGuia():
+    file_type = (('XML files', '*.xml'), ('All files', '*.*'))
+    guide_path = fd.askopenfilename(filetypes=file_type)
+    if os.path.isfile(guide_path):
+        conta = Conta(guide_path)
+        return conta
+    else:
+        mb.showwarning(title='Erro', message='A guia não foi escolhida!')
+
+
+def alteraDados(conta):
+    global variavel_de_controle
+    variavel_de_controle = 0
+    tabela = Tabela()
+    for linha in tabela.getTabelaDados():
+        numero_guia, codigoDeProcedimento, novoCodigoDeProcedimento, tipoDeTabela, novoTipoDeTabela, \
+        grauDeParticipacao, novoGrauDeParticipacao, codigoDeDesepsa, novoCodigoDeDespesa, unidadeDeMedida, \
+        novoUnidadeDeMedida = setLinhaAlteracaoDeDados(linha)
+
+        for item in conta.guias:
+            guia = Guia(item, codigoDeProcedimento)
+
+            print('----------- ' + guia.getNumeroGuia() + ' -----------')
+            if Et.iselement(guia.getProcedimentosExecutados()):
+                for procedimento in guia.getListaProcedimento():
+                    p = Procedimento(procedimento, guia)
+                    p.alteraCodigoProcedimentoExecutado(codigoDeProcedimento, novoCodigoDeProcedimento)
+                    p.alteraGrauDeParticipacao(grauDeParticipacao, novoGrauDeParticipacao)
+                    p.alteraCodigoDeTabela(tipoDeTabela, novoTipoDeTabela)
+                    variavel_de_controle += p.qtd_alteracoes
+
+            if Et.iselement(guia.getDespesas()):
+                for procedimento in guia.getListaDespesa():
+                    p = Procedimento(procedimento, guia)
+                    p.alteraCodigoProcedimentoDespesa(codigoDeProcedimento, novoCodigoDeProcedimento)
+                    p.alteraCodigoDeDespesa(codigoDeDesepsa, novoCodigoDeDespesa)
+                    p.alteraCodigoDeTabela(tipoDeTabela, novoTipoDeTabela)
+                    p.alteraUnidadeDeMedida(unidadeDeMedida, novoUnidadeDeMedida)
+                    variavel_de_controle += p.qtd_alteracoes
+
+
+def alteraValores(conta):
+    global variavel_de_controle
+    variavel_de_controle = 0
+    tabela = Tabela()
+    for linha in tabela.getTabelaValores():
+        numero_guia, codigoDeProcedimento, valor, novoValor = setLinhaAlteracaoDeValores(linha)
+
+        for item in conta.guias:
+            guia = Guia(item, codigoDeProcedimento)
+
+            print('----------- ' + guia.getNumeroGuia() + ' -----------')
+            if Et.iselement(guia.getProcedimentosExecutados()):
+                for procedimento in guia.getListaProcedimento():
+                    p = Procedimento(procedimento, guia)
+                    p.alteraValorUnitario(valor, novoValor)
+                    variavel_de_controle += p.qtd_alteracoes
+
+            if Et.iselement(guia.getDespesas()):
+                for procedimento in guia.getListaDespesa():
+                    p = Procedimento(procedimento, guia)
+                    p.alteraValorUnitario(valor, novoValor)
+                    variavel_de_controle += p.qtd_alteracoes
+
